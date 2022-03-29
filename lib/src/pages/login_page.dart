1d9875642d78001b876486627b7d5ea2ae911b69
Diana Gonzalez <estephanie.ti20@utsjr.edu.mx>
2022-03-29 15:15:59 -0600

    Actualización del login

--- lib/src/pages/login_page.dart
+++ lib/src/pages/login_page.dart
@@ -5,19 +5,110 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final _width = MediaQuery.of(context).size.width;
+    final _height = MediaQuery.of(context).size.height;
     return Scaffold(
       body: Stack(
-        alignment: AlignmentDirectional.topCenter,
-        children: <Widget>[
-          Image.asset('lib/src/img/subtraction_2.png',
-          fit: BoxFit.fill,
-          width: _width,
-          ),
-          Image.asset('lib/src/img/subtraction_1.png',
-          fit: BoxFit.fill,
-          width: _width,
-          height: 350.0,
+        children: <Widget> [
+          Column(
+            children: <Widget> [
+              SizedBox(
+                width: double.infinity,
+                height: _height *0.5,
+                //color: Colors.yellow,
+                child: Stack(
+                  children: <Widget>[
+                    Container(
+                      decoration: const BoxDecoration(
+                        image: DecorationImage(
+                          fit: BoxFit.cover,
+                          image: AssetImage('lib/src/img/subtraction_2.png')
+                          )
+                      ),
+                    ),
+                    Container(
+                      height: _height*0.44,
+                      decoration: const BoxDecoration(
+                        image: DecorationImage(
+                          fit: BoxFit.cover,
+                          image: AssetImage('lib/src/img/subtraction_1.png')
+                          )
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              SizedBox(
+                width: double.infinity,
+                height: _height *0.5,
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      margin: const EdgeInsets.only(top: 150.0),
+                      child: Wrap(
+                        alignment: WrapAlignment.center,
+                        children: const <Widget>[
+                          Text('Discover the nest foods from over 1,00',
+                            style: TextStyle(
+                              color: Color.fromRGBO(124, 125, 126, 1.0),
+                              fontSize: 14.0
+                            ),
+                          ),
+                          SizedBox(height: 25.0),
+                          Text('restaurants and gast delivery to your doorstep',
+                            style: TextStyle(
+                              color: Color.fromRGBO(124, 125, 126, 1.0),
+                              fontSize: 14.0
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                    const SizedBox(height: 20.0),
+                    Container(
+                      width: double.infinity,
+                      height: 55.0,
+                      margin: const EdgeInsets.only(left: 40.0, right: 40.0),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(30),
+                        color: Theme.of(context).primaryColor
+                      ),
+                      child: const TextButton(
+                        onPressed: null, 
+                        child: Text('Login',
+                          style: TextStyle(
+                            fontSize: 18.0,
+                            color: Colors.white
+                          ),
+                        )
+                      ),
+                    ),
+                    const SizedBox(height: 10.0),
+                    Container(
+                      width: double.infinity,
+                      height: 55.0,
+                      margin: const EdgeInsets.only(left: 40.0, right: 40.0),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(30),
+                        border: Border.all(
+                          color: Theme.of(context).primaryColor,
+                          width: 2.0
+                        )
+                        //color: Theme.of(context).primaryColor
+                      ),
+                      child: TextButton(
+                        onPressed: null, 
+                        child: Text('Create an Accout',
+                          style: TextStyle(
+                            fontSize: 18.0,
+                            color: Theme.of(context).primaryColor
+                          ),
+                        )
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ],
           ),
           Column(
             mainAxisAlignment: MainAxisAlignment.center,
@@ -54,38 +145,8 @@ class LoginPage extends StatelessWidget {
               const Text('FOOD DELIVERY')
             ],
           ),
-          Positioned(
-            bottom: 100.0,
-            left: 40.0,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              //crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                Container(
-                  width: 300.0,
-                  height: 40.0,
-                  //alignment: AlignmentDirectional.center,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20.0)
-                  ),
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      primary: Theme.of(context).primaryColor,
-                      onPrimary: Colors.white
-                    ),
-                    child: const Text('Entrar',
-                      style: TextStyle(
-                        fontSize: 30.0
-                      ),
-                    ),
-                    onPressed: () => {},
-                  )
-                ),
-              ],
-            ),
-          )
         ],
-      ),
+      )
     );
   }
 }
